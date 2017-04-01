d8c48796f532d52e2c6fc8a82917c6f8f2167ebe
dormd <dormoshed@gmail.com>
2017-04-01 15:02:33 +0300

    Create app-module.v4.ts

---
+++ upgrade/directive/app-module.v4.ts
@@ -0,0 +1,13 @@
+import { HybridHelper } from './hybrid-helper.v4';
+import { DocumentStarsDirective } from './document-stars.v4';
+
+const myDirectives = [
+  DocumentStarsDirective
+];
+
+@NgModule({
+  declarations: [
+    ...myDirectives
+  ],  
+  …
+});
