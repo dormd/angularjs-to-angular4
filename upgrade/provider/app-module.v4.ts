7ef533b088903f31746479daacbebb90cb9fecf2
dormd <dormoshed@gmail.com>
2017-04-01 15:00:14 +0300

    Create app-module.v4.ts

---
+++ upgrade/provider/app-module.v4.ts
@@ -0,0 +1,13 @@
+import { HybridHelper } from '../hybrid-helper';
+
+const myServices = [
+  HybridHelper.buildProviderForUpgrade('DocumentConverter')
+];
+
+@NgModule({
+  // services
+  providers: [
+    ...myServices
+  ],  
+//  …
+});
