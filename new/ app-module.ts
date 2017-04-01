84819e82244c9f25f9f6f6867d243fadfd361f20
dormd <dormoshed@gmail.com>
2017-04-01 14:48:37 +0300

    Create  app-module.ts

---
+++ new/ app-module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { UpgradeModule } from '@angular/upgrade/static';
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    UpgradeModule
+  ],
+  bootstrap: []
+})
+export class AppModule {
+  ngDoBootstrap() {}
+}
