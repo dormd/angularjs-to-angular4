721fddfb82ffdbf40a5c140c1288cde48fab7049
dormd <dormoshed@gmail.com>
2017-04-01 14:44:16 +0300

    Create upgrade-adapter.old.ts

---
+++ old/upgrade-adapter.old.ts
@@ -0,0 +1,13 @@
+import { NgModule, forwardRef } from '@angular/core';
+import { UpgradeAdapter } from '@angular/upgrade';
+import { BrowserModule } from '@angular/platform-browser';
+
+export const upgradeAdapter = new UpgradeAdapter(forwardRef(() => AppModule));
+
+@NgModule({
+  imports: [
+    BrowserModule
+  ],
+  bootstrap: []
+})
+export class AppModule {}
